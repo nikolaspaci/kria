3942d2d252d99c0282c86fb2b9419dce65183091
nikolaspaci <nikolaspaci@gmail.com>
2025-08-15 23:15:09 +0200

    Pass ModelParameters object to JNI methods
    The `predict` and `init` JNI methods now accept a `ModelParameters` Java object as an argument. This allows for passing model-specific parameters from Java to the native C++ code.

--- llamaCpp/include/JNIMethods/PredictJni.hpp
+++ llamaCpp/include/JNIMethods/PredictJni.hpp
@@ -3,6 +3,6 @@
 #include <jni.h>
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_nikolaspaci_app_llamallmlocal_LlamaApi_predict(JNIEnv *env,jobject /* this */,jlong session_ptr,jstring prompt_j,jobject callback_obj) ;
+Java_com_nikolaspaci_app_llamallmlocal_LlamaApi_predict(JNIEnv *env,jobject /* this */,jlong session_ptr,jstring prompt_j,jobject modelParameters,jobject callback_obj) ;
 
 #endif // PREDICT_JNI_HPP
